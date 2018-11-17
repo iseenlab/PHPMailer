b369b9669d47db9d2befdf3bae694f2332e48bbb
iseenlab <45116131+iseenlab@users.noreply.github.com>
2018-11-17 11:36:42 +0530

    mail.php
    To understand or learn how to send mail using php follow video guide & PHP script is given below:
    https://www.youtube.com/watch?v=EM630O5W-_I
    
    
    Credit: https://www.domainracer.com/

---
+++ mail.php
@@ -0,0 +1,57 @@
+<?php
+require "PHPMailer/PHPMailerAutoload.php";
+
+function smtpmailer($to, $from, $from_name, $subject, $body)
+    {
+        $mail = new PHPMailer();
+        $mail->IsSMTP();
+        $mail->SMTPAuth = true; 
+ 
+        $mail->SMTPSecure = 'ENTER SMTP PROTOCOL'; 
+        $mail->Host = 'ENTER SMTP SERVER HOST NAME';
+        $mail->Port = ENTER SMTP PORT NUMBER;  
+        $mail->Username = 'ENTER YOUR MAIL ID';
+        $mail->Password = 'ENTER YOUR EMAIL PASSWORD';   
+   
+   //   $path = 'reseller.pdf';
+   //   $mail->AddAttachment($path);
+   
+        $mail->IsHTML(true);
+        $mail->From="ENTER FROM EMAIL ADDRESS HERE";
+        $mail->FromName=$from_name;
+        $mail->Sender=$from;
+        $mail->AddReplyTo($from, $from_name);
+        $mail->Subject = $subject;
+        $mail->Body = $body;
+        $mail->AddAddress($to);
+        if(!$mail->Send())
+        {
+            $error ="Please try Later, Error Occured while Processing...";
+            return $error; 
+        }
+        else 
+        {
+            $error = "Thanks You !! Your email is sent.";  
+            return $error;
+        }
+    }
+    
+    $to   = '';
+    $from = '';
+    $name = '';
+    $subj = 'PHPMailer 5.2 testing from DomainRacer';
+    $msg = 'This is mail about testing mailing using PHP.';
+    
+    $error=smtpmailer($to,$from, $name ,$subj, $msg);
+    
+?>
+
+<html>
+    <head>
+        <title>PHPMailer 5.2 testing from DomainRacer</title>
+    </head>
+    <body style="background: black;">
+        <center><h2 style="padding-top:70px;color: white;"><?php echo $error; ?></h2></center>
+    </body>
+    
+</html>
